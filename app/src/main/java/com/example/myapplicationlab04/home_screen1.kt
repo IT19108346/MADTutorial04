0b30c9c43f2a4e8ac235d41d757fdab04f92a126
IT19108346 <bingunalahakoon448@gmail.com>
2023-03-10 18:49:36 +0530

    change in second

---
+++ app/src/main/java/com/example/myapplicationlab04/home_screen1.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationlab04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home_screen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen1)
+    }
+}
